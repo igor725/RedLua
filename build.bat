78141219bc90c7ec99f831fe0aef77966f426b00
igor725 <gvaldovigor@gmail.com>
2022-07-20 03:16:55 +0300

    GitHub Actions initial

--- build.bat
+++ build.bat
@@ -5,11 +5,28 @@ IF NOT "%VSCMD_ARG_TGT_ARCH%"=="x64" (
 )
 setlocal enableextensions enabledelayedexpansion
 SET RL_LUAJIT_SOURCE_DIR=.\src\thirdparty\luajit\src
+IF NOT EXIST "%RL_LUAJIT_SOURCE_DIR%\lua51.lib" (
+	PUSHD %RL_LUAJIT_SOURCE_DIR%
+	CALL .\msvcbuild.bat
+	IF NOT "!ERRORLEVEL!"=="0" (
+		ECHO Failed to compile LuaJIT
+		EXIT /b 1
+	)
+	POPD
+)
 SET RL_SCRIPTHOOK_SDK_DIR=.\src\thirdparty\ScriptHook
-SET RL_OUT_PATH="D:\SteamLibrary\steamapps\common\Red Dead Redemption 2\RedLua.asi"
+SET RL_OUT_PATH="D:\SteamLibrary\steamapps\common\Red Dead Redemption 2"
+MKDIR ".\objs\" 2> NUL
+IF NOT EXIST !RL_OUT_PATH! (
+	SET RL_OUT_PATH=".\objs\output"
+	MKDIR !RL_OUT_PATH! 2> NUL
+)
+IF NOT EXIST "!RL_OUT_PATH!\lua51.dll" (
+	COPY %RL_LUAJIT_SOURCE_DIR%\lua51.dll %RL_OUT_PATH% 2> NUL
+)
 SET RL_CFLAGS=/DELPP_NO_DEFAULT_LOG_FILE /DELPP_DISABLE_LOG_FILE_FROM_ARG /W2 ^
-/Isrc\ /EHsc /MP /MT /DLL /Foobjs\ /Fe%RL_OUT_PATH%
-SET RL_LDFLAGS=/incremental /libpath:"%RL_LUAJIT_SOURCE_DIR%" /libpath:"%RL_SCRIPTHOOK_SDK_DIR%\lib" /dll
+/Isrc\ /EHsc /MP /MT /DLL /Foobjs\ /Fe!RL_OUT_PATH!\RedLua.asi
+SET RL_LDFLAGS=/INCREMENTAL /LIBPATH:"%RL_LUAJIT_SOURCE_DIR%" /LIBPATH:"%RL_SCRIPTHOOK_SDK_DIR%\lib" /dll
 SET RL_LIBS=ScriptHookRDR2.lib user32.lib shell32.lib lua51.lib
 SET RL_SOURCES=src\*.cpp src\thirdparty\*.cpp src\menus\*.cpp
 CL %RL_CFLAGS% %RL_SOURCES% /link %RL_LDFLAGS% %RL_LIBS%
---
+++ misc/ScriptHook.ps1
@@ -0,0 +1,97 @@
+$SCR_DIR = $(Split-Path $MyInvocation.MyCommand.Path -Parent)
+$SHOOK_DIR = "$SCR_DIR/../src/thirdparty/ScriptHook"
+$DOMAIN = "http://www.dev-c.com"
+$LINK = "/rdr2/scripthookrdr2/"
+$FILES = @(
+	"inc/", "lib/",
+	"inc/enums.h", "inc/main.h", "inc/nativeCaller.h", "inc/natives.h",
+	"inc/types.h", "lib/ScriptHookRDR2.lib", "readme.txt"
+)
+$PATCHES = @{
+	"inc/main.h" = @(
+		,@(
+			"VER_1_0_1207_69_RGS,`r`n`r`n`tVER_SIZE",
+			("`VER_1_0_1207_69_RGS,`r`n" +
+			"`r`n`t//Added by patcher`r`n" +
+			"`tVER_1_0_1207_73_RGS,`r`n" +
+			"`tVER_1_0_1207_77_RGS,`r`n" +
+			"`tVER_1_0_1207_80_RGS,`r`n" +
+			"`tVER_1_0_1232_13_RGS,`r`n" +
+			"`tVER_1_0_1232_17_RGS,`r`n" +
+			"`tVER_1_0_1311_12_RGS,`r`n" +
+			"`tVER_1_0_1436_25_RGS,`r`n" +
+			"`tVER_1_0_1436_31_RGS,`r`n" +
+			"`r`n`tVER_SIZE")
+		)
+	)
+}
+
+if(Test-Path -PathType Container $SHOOK_DIR) {
+	$corrupted = $false
+
+	Foreach($item in $FILES) {
+		if(!(Test-Path "$SHOOK_DIR/$item")) {
+			$corrupted = $true;
+			break
+		}
+	}
+
+	if($corrupted) {
+		$title = "Corrupted ScriptHookRDR2 installation"
+		$text = "Looks like you have a broken ScriptHookRDR2, do you want to redownload it?"
+		$choices = New-Object Collections.ObjectModel.Collection[Management.Automation.Host.ChoiceDescription]
+		$choices.Add((New-Object Management.Automation.Host.ChoiceDescription -ArgumentList "&Yes", "ScriptHookRDR2 will be redownloaded"))
+		$choices.Add((New-Object Management.Automation.Host.ChoiceDescription -ArgumentList "&No", "This script will close"))
+		switch($Host.UI.PromptForChoice($title, $text, $choices, 1)) {
+			0 {
+				Remove-Item -Recurse -Force $SHOOK_DIR
+			}
+			1 {
+				Write-Host "Operation cancelled"
+				Exit 0
+			}
+		}
+	} else {
+		Exit 0
+	}
+} else {
+	New-Item -ItemType Directory -Path $SHOOK_DIR
+}
+
+
+$HEADERS = @{
+	"Referer" = ($DOMAIN + $LINK);
+	"User-Agent" = "RL/1.0"
+}
+
+Foreach($elem in (Invoke-WebRequest -URI ($DOMAIN + $LINK)).Links.Href) {
+	if($elem.IndexOf("ScriptHookRDR2_SDK") -ige 0) {
+		$outFile = "$SCR_DIR/temp.zip";
+		Invoke-WebRequest -Uri ($DOMAIN + $elem) -OutFile $outFile -Headers $HEADERS
+		Add-Type -Assembly System.IO.Compression.FileSystem
+		$zip = [IO.Compression.ZipFile]::OpenRead($outFile)
+		Foreach($ent in $zip.Entries) {
+			$fileName = $ent.FullName
+			if($FILES.Contains($fileName)) {
+				if($fileName.Substring($fileName.Length - 1) -eq '/') {
+					New-Item -ItemType Directory -Path "$SHOOK_DIR\$filename"
+					Continue
+				}
+				[IO.Compression.ZipFileExtensions]::ExtractToFile($ent, "$SHOOK_DIR\$fileName", $true)
+			}
+		}
+		$zip.Dispose()
+		Foreach($fpatches in $PATCHES.GetEnumerator()) {
+			$fileName = "$SHOOK_DIR/$($fpatches.Name)"
+			$content = (Get-Content $fileName -Raw)
+			Foreach($patch in $fpatches.Value) {
+				$content = $content -replace $patch[0], $patch[1]
+			}
+			$content | Set-Content $fileName -Force
+		}
+		Remove-Item -Path $outFile
+		Exit 0
+	}
+}
+
+Write-Error "SDK link not found"
